c1b9ebffbdabf8425cc3248bcc7f5d80b4bf2111
Pedro <pedro>
2019-01-08 15:04:28 +0000

    console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aws_ci_cd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
